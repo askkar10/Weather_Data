bf88f8a9a67e817caeeae299344f4b55fe0abf10
Askaniusz <askkar27@icloud.com>
2025-07-30 11:54:00 +0200

    parsing the inventory data
    use dataclass to create a custom inventory class, parse inventory lines and convert some calues to float and int, print some lines to check the data

--- weather.py
+++ weather.py
@@ -22,3 +22,24 @@ stations_txt = r.text
 
 #with open("Weather_data/stations.txt", "w",encoding="utf-8") as stations_file:
 #    stations_file.write(stations_txt)
+
+# parsing the inventory data
+# use dataclass to create a custom inventory class
+
+from dataclasses import dataclass
+@dataclass
+class Inventory:
+    station: str
+    latitude: float
+    longitude: float
+    element: str
+    start: int
+    end: int
+
+# parse inventory lines and convert some values to floats and ints
+inventory = [Inventory(x[0:11], float(x[12:20]), float(x[21:30]), x[31:35],
+                       int(x[36:40]), int(x[41:45])) for x in inventory_txt.split("\n")
+                       if x.startswith("US")]
+
+for line in inventory[:10]:
+    print(line)
